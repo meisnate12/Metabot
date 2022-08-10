9210f6f92e920c48d8a3f838cd7dfc240bfb78dc
meisnate12 <meisnate12@gmail.com>
2022-08-10 11:08:08 -0400

    add !close

--- main.py
+++ main.py
@@ -27,16 +27,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        print(message.channel.type)
-        await message.channel.send('Hello!', view=Buttons())
+    if message.content.startswith('!close') and hasattr(message.channel, "parent_id") and message.channel.parent_id in [1006644783743258635, 1006477130617593906]
+        await message.channel.edit(auto_archive_duration=60)
+        await message.channel.send("This post has been marked as Resolved and will be closed in one hour.\n\nIf you require further assistance on this issue, you can send another message in this post to re-open it.\n\nIf you require further support that is unrelated to this post, please start a new post and do not re-open this one.\n\nThanks for using Plex Meta Manager.")
 
 
 @client.event
 async def on_thread_create(thread):
-    print("created")
-    print(thread.parent_id)
-    if thread.parent_id == 1006644783743258635:
+    if thread.parent_id in [1006644783743258635, 1006477130617593906]:
         await thread.send("Please complete <#938455615741775902> and someone from the community or one of our <@&938443185347244033> members will respond when they're available.\n\nIncluding the meta.log from the beginning is a huge help use !logs for more information.", view=Buttons())
 
 client.run(os.getenv('DISCORD_TOKEN'))
